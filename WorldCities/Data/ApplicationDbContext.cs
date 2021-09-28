01649fc9ab8bd877da528eff379e1cb8175227a4
Getulio Fagundes Junior <getuliojuniorfagundes@gmail.com>
2021-09-28 09:51:29 -0300

    Imp Fluent API

--- WorldCities/Data/ApplicationDbContext.cs
+++ WorldCities/Data/ApplicationDbContext.cs
@@ -8,13 +8,22 @@ namespace WorldCitites.Data
         public ApplicationDbContext() : base()
         {}
         
-        public ApplicationDbContext(DbContextOptions options)
-        {}
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<City>().HasKey(x => x.Id);
+            modelBuilder.Entity<City>().Property(x => x.Id).IsRequired();
+
+            modelBuilder.Entity<City>().ToTable("Countries");
+            modelBuilder.Entity<Country>().HasKey(x => x.Id);
+            modelBuilder.Entity<Country>().Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>().HasOne(x => x.Country).WithMany(y => y.Cities).HasForeignKey(x => x.CountryId);
         }
+
+        public ApplicationDbContext(DbContextOptions options)
+        {}
 
         public DbSet<City> Cities {get; set;}
         public DbSet<Country> Countries {get; set;}
